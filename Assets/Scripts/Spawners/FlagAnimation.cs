6eb12e8899734285c78a98188fae5ddc67bf1232
Akkuun <mathisduban@gmail.com>
2022-08-07 23:36:59 +0200

    flag getting fix

--- Assets/Scripts/Spawners/FlagAnimation.cs
+++ Assets/Scripts/Spawners/FlagAnimation.cs
@@ -1,25 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class FlagAnimation : MonoBehaviour
 {
     public Animator m_animator;
-
-    // Start is called before the first frame update
+    private GameObject[] goArray;
+   
     void Start()
     {
-        
+            GameObject[] gameObjectArray = GameObject.FindGameObjectsWithTag("Checkpoint");
+  
+        goArray = gameObjectArray ;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+
+     }
+
+
 
     private void OnTriggerEnter2D(Collider2D collision)
+
     {
-        m_animator.SetBool("isActivated", true);
+        foreach(GameObject  go in goArray){
+            if(go.name != "Checkpoint")
+            {
+                Debug.Log(Mathf.Round(go.transform.position.x)+"  |  "+Mathf.Round(transform.position.x));
+                if(Mathf.Round(go.transform.position.x) != Mathf.Round(transform.position.x))
+                {
+                     go.GetComponent<Animator>().SetBool("isActivated", false);
+                }
+                else
+                {
+                    go.GetComponent<Animator>().SetBool("isActivated", true);
+                }
+            }
+        }
     }
 }
