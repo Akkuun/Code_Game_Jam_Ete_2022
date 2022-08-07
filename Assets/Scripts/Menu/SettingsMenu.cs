39b3daf6625435c466b1514eb2cab0608e64af94
thi-christine-nguyen <thichristine@icloud.com>
2022-08-07 15:14:59 +0200

    menu

---
+++ Assets/Scripts/Menu/SettingsMenu.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public void setFullScreen(bool isFullScreen){
+        Screen.fullScreen = isFullScreen;
+    }
+}
